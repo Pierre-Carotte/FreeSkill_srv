c2456e098167ea42dd19a698aaf2e52c61f8c6ae
sofatra <zeux59200@hotmail.fr>
2017-11-30 13:41:44 +0100

    utils functions

--- routes/user.js
+++ routes/user.js
@@ -5,17 +5,12 @@ var bodyParser = require('body-parser');
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
-console.log(bodyParser.json());
+
 router.use(function (req, res, next) {
-    console.log("middleware: json web token verify");
-    console.log('Time:', Date.now());
     next();
 });
 
-
 var searchProfiles = require('../api/user/searchProfiles');
 router.use('/', searchProfiles);
-
-
 
 module.exports = router;
